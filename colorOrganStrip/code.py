a78c32232525b110e6c34d7692ab6517a290df82
Paul V. Miller <86619288+lauprellim@users.noreply.github.com>
2025-04-10 10:42:18 -0400

    Add files via upload

--- colorOrganStrip/code.py
+++ colorOrganStrip/code.py
@@ -18,8 +18,13 @@ serial = usb_cdc.data
 numPixels = 90
 pixels = neopixel.NeoPixel(board.A1,numPixels, brightness=0.5, auto_write=False)
 
-# pixelArray = [[0 for x in range(numPixels)] for y in range(3)]
 pixelArray = [[0 for  x in range(3)] for y in range(numPixels)]
+
+def movePixels(numPixels):
+    for i in range(numPixels):
+        pixelArray[numPixels-i-1][0] = pixelArray[numPixels-i-2][0]
+        pixelArray[numPixels-i-1][1] = pixelArray[numPixels-i-2][1]
+        pixelArray[numPixels-i-1][2] = pixelArray[numPixels-i-2][2]
 
 while True:
 
@@ -30,14 +35,19 @@ while True:
         
         print(data_in)
         values = data_in.split()
-        # address = int(values[0])
         r = int(values[1])
         g = int(values[2])
         b = int(values[3])
         
         COLOR = (r, g, b)
-                
-        pixelArray.insert( 0, COLOR )
+        
+        # move the second-to-last pixel to the last, and so on...
+        movePixels(numPixels)
+        # add the new value to the beginning of the strip
+        pixelArray[0][0] = r
+        pixelArray[0][1] = g
+        pixelArray[0][2] = b
+        
         print("-----")
         print(pixelArray)
         print("-----")
