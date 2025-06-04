9392f91a8fc4f262267e53a8d7a754ec027bdc37
Paul V. Miller <86619288+lauprellim@users.noreply.github.com>
2025-06-04 14:18:28 -0400

    Add files via upload

--- temperature-humidity/pico/code.py
+++ temperature-humidity/pico/code.py
@@ -25,13 +25,20 @@ sht = adafruit_sht4x.SHT4x(i2c)
 sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 
 while True:
+    #  connect to SSID
+    wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+    
     temp, hum = sht.measurements
     URLtemp = "%3.1f" %temp
     URLhum = "%3.1f" %hum
-    formattedURL = "http://www.theoryofpaul.net/temphumid-duq/enter-data.phtml?temp=" + str(URLtemp) + "&humidity=" + str(URLhum) + "&idLocations=1&duqPassword=xxxxxxxx"
-
+    formattedURL = "http://www.theoryofpaul.net/temphumid-duq/enter-data.phtml?temp=" + str(URLtemp) + "&humidity=" + str(URLhum) + "&idLocations=1&duqPassword=duQuesne1878!"
+        
     response = requests.get(formattedURL)
     response.close()
 
     # 60 times 15 = 900 seconds
     time.sleep(900)
+ 
