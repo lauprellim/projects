adb5bde81ac5ef65a0de8ee68f815b88e59ee982
Paul V. Miller <86619288+lauprellim@users.noreply.github.com>
2025-06-05 08:43:09 -0400

    Add files via upload

--- temperature-humidity/pico/code.py
+++ temperature-humidity/pico/code.py
@@ -1,6 +1,6 @@
 # Written by Paul V. Miller
 # Media Maker's Lab 2025 Project
-# Be sure to set the "idLocations" variable in line 32 (formattedURL = ...)
+# Be sure to set the "idLocations" variable in the URL
 
 import os
 import time
@@ -14,25 +14,46 @@ import board
 import busio
 import adafruit_sht4x
 
+# Initialize I2C and sensor
 i2c = busio.I2C(scl=board.GP1, sda=board.GP0)
 sht = adafruit_sht4x.SHT4x(i2c)
-
 sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
+# Let the sensor settle
+time.sleep(1)
 
+# Connect to WiFi
+# print("Connecting to WiFi...")
+wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+# print("Connected to WiFi.")
+
+# Prepare request session
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl.create_default_context())
+
+# Main loop
 while True:
-    #  connect to SSID
-    wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+    try:
+        # Read sensor data
+        temp, hum = sht.measurements
+        URLtemp = "%3.1f" %temp
+        URLhum = "%3.1f" %hum
 
-    pool = socketpool.SocketPool(wifi.radio)
-    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+        # Construct request URL
+        formattedURL = (
+            "http://www.theoryofpaul.net/temphumid-duq/enter-data.phtml?"
+            f"temp={URLtemp}&humidity={URLhum}&idLocations=1&duqPassword=duQuesne1878!"
+        )
+        print(f"Sending data: Temp={URLtemp}°C, Humidity={URLhum}%")
 
-    temp, hum = sht.measurements
-    URLtemp = "%3.1f" %temp
-    URLhum = "%3.1f" %hum
-    formattedURL = "http://www.theoryofpaul.net/temphumid-duq/enter-data.phtml?temp=" + str(URLtemp) + "&humidity=" + str(URLhum) + "&idLocations=1&duqPassword=duQuesne1878!"
+        # Send GET request
+        response = requests.get(formattedURL)
+        response.close()
+        # print("Data sent successfully.")
 
-    response = requests.get(formattedURL)
-    response.close()
+    except Exception as e:
+        # print("Error occurred:", e)
+        # Reset device if request failed
+        microcontroller.reset()
 
-    # 60 times 15 = 900 seconds
+    # Wait 15 minutes before next reading
     time.sleep(900)
